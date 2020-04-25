2a80cd61ad3b7dd7d5dae6780b815ef1d9dbb998
Julian Bustamante <jbustamante35@gmail.com>
2020-04-25 11:17:21 -0500

    Updated for 4-25-2020

--- analysis.m
+++ analysis.m
@@ -4,7 +4,7 @@
 % Author Julian Bustamante <jbustamante@wisc.edu>
 
 %% Import and process data
-din = '/home/jbustamante/Dropbox/Misc/dataanalytics/vertical';
+din = '/home/jbustamante/Dropbox/Misc/dataanalytics/ujumpgood';
 fin = 'data.csv';
 w   = 'weather_code.txt';
 deg = char(176);
@@ -41,50 +41,121 @@ dtstr  = cellfun(@(x) [x(4:5) , '-' , x(1:2)], ...
 tstr   = cellfun(@(x) sprintf('%s%s', x, deg), ...
     cellstr(num2str(Y.Temperature_C)), 'UniformOutput', 0);
 
+% Interpolate data points
+% tst               = ...
+%     interpolateOutline([Y.Measurement Y.Vertical_cm], numel(tt.Time));
+repl               = fillmissing(tt.Var1, 'linear');
+% repl              = tt.Var1;
+% repl(isnan(repl)) = tst(isnan(repl),2);
+
 %% Plot data to test this out
 size_data  = 12;
 size_title = 11;
 size_label = 10;
 size_ticks = 10;
+mskip      = 2;
+mbuff      = 1;
+tbuff      = 1;
+wbuff      = 0.2;
 
 figclr(1);
+fnms{1} = sprintf('%s_VerticalAnalysis_Day%d', tdate, max(Y.Measurement));
 
+% ------------------------------- Measurement -------------------------------- %
 subplot(221);
 hold on;
+plt(repl, 'r.', size_data - 2);
 plt(tt.Var1, 'k.', size_data);
-plt([0 , GOAL ; numel(tt.Time) , GOAL], 'r-', 1);
-xticks(1 : numel(tt.Time));
-xticklabels(dtstr);
-ylim(round([min(tt.Var1)-1 , GOAL+1]));
-set(gca, 'FontSize', size_ticks-2);
+plt([0 , GOAL ; numel(tt.Time) , GOAL], 'r--', 1);                             % Goal
+plt([0 , Y.Vertical_cm(end) ; numel(tt.Time) , Y.Vertical_cm(end)], 'b-', 1); % Latest
+plt([0 , Y.Vertical_cm(1) ; numel(tt.Time) , Y.Vertical_cm(1)], 'm-', 1);     % Initial
+xticks(1 : mskip : numel(tt.Time));
+xticklabels(dtstr(cellfun(@str2num, get(gca, 'XTickLabel'))));
+ylim(round([min(tt.Var1) - mbuff , GOAL + mbuff]));
+set(gca, 'FontSize', size_ticks - 2);
 xlabel('Date', 'FontSize', size_label);
 ylabel('Vertical (cm)', 'FontSize', size_label);
 title('Timeline', 'FontSize', size_title);
 
+% ---------------------------------- Weight ---------------------------------- %
 subplot(222);
+hold on;
 plt([Y.Weight_kg , Y.Vertical_cm], 'k.', size_data);
+plt([Y.Weight_kg(end) , Y.Vertical_cm(end)], 'b.', size_data + 1); % Latest
+plt([Y.Weight_kg(1) , Y.Vertical_cm(1)], 'm.', size_data + 1);     % Initial
 set(gca, 'FontSize', size_ticks);
-xlim(round([min(Y.Weight_kg) , max(Y.Weight_kg)]));
-% xlabel('Weight (kg)', 'FontSize', size_label);
+xlim([min(Y.Weight_kg) - wbuff, max(Y.Weight_kg) + wbuff]);
 ylabel('Vertical (cm)', 'FontSize', size_label);
 title('Weight (kg)', 'FontSize', size_title);
 
+% ------------------------------- Temperature -------------------------------- %
 subplot(223);
+hold on;
 plt([Y.Temperature_C, Y.Vertical_cm], 'k.', size_data);
+plt([Y.Temperature_C(end) , Y.Vertical_cm(end)], 'b.', size_data + 1); % Latest
+plt([Y.Temperature_C(1) , Y.Vertical_cm(1)], 'm.', size_data + 1);     % Initial
 set(gca, 'FontSize', size_ticks);
-% xlabel('Temperature (C)', 'FontSize', size_label);
 ylabel('Vertical (cm)', 'FontSize', size_label);
+xlim([min(Y.Temperature_C) - tbuff , max(Y.Temperature_C) + tbuff]);
 title(sprintf('Temperature (%sC)', deg), 'FontSize', size_title);
 
+% ---------------------------------- Weather --------------------------------- %
 subplot(224);
+hold on;
 plt([Y.WeatherCode, Y.Vertical_cm], 'k.', size_data);
-text(Y.WeatherCode + 0.1, Y.Vertical_cm, tstr, ...
-    'Color', 'k', 'FontSize', 8);
+plt([Y.WeatherCode(end) , Y.Vertical_cm(end)], 'b.', size_data + 1); % Latest
+plt([Y.WeatherCode(1) , Y.Vertical_cm(1)], 'm.', size_data + 1);     % Initial
+text(Y.WeatherCode + 0.1, Y.Vertical_cm, tstr, 'Color', 'k', 'FontSize', 8);
 set(gca, 'FontSize', size_ticks);
-% xlabel('Weather', 'FontSize', size_label);
 ylabel('Vertical (cm)', 'FontSize', size_label);
 xticklabels(W.textdata);
 xticks(1 : max(Y.WeatherCode));
 xlim([0 , numel(W.textdata) + 1]);
 title('Weather', 'FontSize', size_title);
+
+%% Projections to new goal
+NEW_GOAL   = 75;
+size_data  = 12;
+size_title = 11;
+size_label = 10;
+size_ticks = 10;
+mskip      = 2;
+mbuff      = 1;
+tbuff      = 1;
+wbuff      = 0.2;
+
+figclr(2);
+fnms{2} = sprintf('%s_VerticalAnalysis_Projection', tdate, max(Y.Measurement));
+
+% ------------------------------- Measurement -------------------------------- %
+hold on;
+plt(repl, 'r.', size_data - 2);
+plt(tt.Var1, 'k.', size_data);
+plt([0 , GOAL ; numel(tt.Time) , GOAL], 'r--', 1);                            % Goal
+plt([0 , Y.Vertical_cm(end) ; numel(tt.Time) , Y.Vertical_cm(end)], 'b-', 1); % Latest
+plt([0 , Y.Vertical_cm(1) ; numel(tt.Time) , Y.Vertical_cm(1)], 'm-', 1);     % Initial
+xticks(1 : mskip : numel(tt.Time));
+xticklabels(dtstr(cellfun(@str2num, get(gca, 'XTickLabel'))));
+ylim(round([min(tt.Var1) - mbuff , NEW_GOAL + mbuff]));
+set(gca, 'FontSize', size_ticks - 2);
+xlabel('Date', 'FontSize', size_label);
+ylabel('Vertical (cm)', 'FontSize', size_label);
+title('Timeline', 'FontSize', size_title);
+
+% -------------------------------- Projection -------------------------------- %
+%% Plot projection until next goal
+max_proj = 32;
+rng_proj = (numel(repl) : max_proj)';
+
+lx  = [(1:numel(repl))' , repl];
+lt  = fitlm(lx(:,1), lx(:,2), 'linear');
+ly  = lt.predict(rng_proj);
+% prj = [(1 : max_proj)' , [lx(:,2) ; ly]];
+prj = [rng_proj , ly];
+
+plot(lt);
+plt(prj, 'k-', 1);
+plt([0 , NEW_GOAL ; max_proj , NEW_GOAL], 'g--', 1);                            % New Goal
+xlim([0 , max_proj]);
+
 
